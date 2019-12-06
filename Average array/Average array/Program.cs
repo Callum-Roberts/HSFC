7f9dd182243518ca5d3feccefeb1016b14b01319
Callum Roberts <cmr9072@hereford.ac.uk>
2019-12-06 15:50:04 +0000

    tesr

--- Average array/Average array/Program.cs
+++ Average array/Average array/Program.cs
@@ -19,7 +19,8 @@ namespace Average_array
 
             Average = ((nums[0] + nums[1] + nums[2] + nums[3] + nums[4] + nums[5] + nums[6] + nums[7] + nums[8] + nums[9])/10);
             Console.WriteLine("Your avarage mark = " + Average);
-
+           
+            Console.ReadLine();
             Console.ReadLine();
         }
         }
