49c163b9c79966e1d00139209996c030be60b7e2
Callum Roberts <cmr9072@hereford.ac.uk>
2020-01-21 14:44:03 +0000

    s

---
+++ linearSearch/linearSearch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace linearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[10];
+            string tempName;
+
+            Console.WriteLine("Please type in names, ending with STOP");
+            bool more = true;
+            int count = 0;
+
+            while (more)|| (count < 10);
+            {
+
+            }
+
+        }
+    }
+}
