695d25dde12bd5b6346246da270251187842bace
Callum Roberts <cmr9072@hereford.ac.uk>
2020-01-28 14:03:41 +0000

    test

--- linearSearch/linearSearch/Program.cs
+++ linearSearch/linearSearch/Program.cs
@@ -13,13 +13,16 @@ namespace linearSearch
             bool more = true;
             int count = 0;
 
-            while (more) &(count < 10);
+            while ((more) & (count < 10))
             {
                 tempName = Console.ReadLine();
                 if (tempName == "STOP")
                     more = false;
                 else
+                {
                     names[count] = tempName;
+                    count = count + 1;
+                }
             }
 
             string nameToSearch;
@@ -29,23 +32,30 @@ namespace linearSearch
             {
                 Console.WriteLine("Please enter name to seach for");
                 nameToSearch = Console.ReadLine();
-                if (nameToSearch = "STOP") 
-                more = false;
-
+                if (nameToSearch == "STOP")
+                
+                    more = false;
+                
                 else
                 {
                     bool found = false;
                     int position = 0;
 
-                    while (!found) &(position < count);
+                    while ((!found) &(position < count))
                     {
                         if (nameToSearch == names[position])
                         {
                             Console.WriteLine(nameToSearch + "is in position" + position);
-                            found = true;
-                            position = position + 1;
+                            found = true;                  
                         }
+                        else
+                            position = position + 1;
                     }
+                    if (!found)
+                        Console.WriteLine("The name wasnt found ");
+                        
+
+                    
 
                 }
             }
